f1b1960b9dd6bf90e6d35c9be0a8bae19d5ec03e
Parth <86726240+parth-deepsource@users.noreply.github.com>
2022-11-30 15:02:54 +0530

    chore: format with black

--- integrations/create_slack_app.py
+++ integrations/create_slack_app.py
@@ -32,44 +32,47 @@ def print_long(text):
         print(fill(line, width=80, replace_whitespace=False))
     print("")
 
-SLACK_APP_NAME = ask("Enter a name for the Slack app (press Enter for \"DeepSource Enterprise\"): ")
+
+SLACK_APP_NAME = ask(
+    'Enter a name for the Slack app (press Enter for "DeepSource Enterprise"): '
+)
 if not SLACK_APP_NAME:
-  SLACK_APP_NAME = "DeepSource Enterprise"
+    SLACK_APP_NAME = "DeepSource Enterprise"
 
 DEEPSOURCE_INSTANCE_URI = input_url(
     "Enter the URL of the DeepSource instance (e.g., https://deepsource.example.com): "
 )
 if not DEEPSOURCE_INSTANCE_URI:
-  print(f"{Colors.RED}Please provide the DeepSource instance URL!{Colors.ENDC}")
-  exit(1)
+    print(f"{Colors.RED}Please provide the DeepSource instance URL!{Colors.ENDC}")
+    exit(1)
 
 SLACK_APP_MANIFEST = dedent(
-  f"""
-  display_information:
-    name: {SLACK_APP_NAME}
-    description: DeepSource Integrations App
-    background_color: "#000000"
-  features:
-    bot_user:
-      display_name: DeepSource Enterprise
-      always_online: true
-  oauth_config:
-    redirect_urls:
-      - {DEEPSOURCE_INSTANCE_URI}/callback/integrations/slack
-    scopes:
-      bot:
-        - channels:read
-        - chat:write
-        - groups:read
-        - chat:write.public
-        - incoming-webhook
-  settings:
-    interactivity:
-      is_enabled: true
-      request_url: {DEEPSOURCE_INSTANCE_URI}/services/integrations/webhooks/slack/
-    org_deploy_enabled: false
-    socket_mode_enabled: false
-    token_rotation_enabled: false
+    f"""
+    display_information:
+      name: {SLACK_APP_NAME}
+      description: DeepSource Integrations App
+      background_color: "#000000"
+    features:
+      bot_user:
+        display_name: DeepSource Enterprise
+        always_online: true
+    oauth_config:
+      redirect_urls:
+        - {DEEPSOURCE_INSTANCE_URI}/callback/integrations/slack
+      scopes:
+        bot:
+          - channels:read
+          - chat:write
+          - groups:read
+          - chat:write.public
+          - incoming-webhook
+    settings:
+      interactivity:
+        is_enabled: true
+        request_url: {DEEPSOURCE_INSTANCE_URI}/services/integrations/webhooks/slack/
+      org_deploy_enabled: false
+      socket_mode_enabled: false
+      token_rotation_enabled: false
   """
 )
 
